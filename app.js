4a2feb528d2c2cc02771a196155f707e773144da
jieul2 <jieul2@kakao.com>
2026-02-13 22:55:16 +0900

    회원가입

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const indexRouter = require("./routes/index");
 const app = express();
 
 require("dotenv").config();
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // req.body가 객체 형태로 인식
+app.use("/api", indexRouter); // /api 경로로 들어오는 요청은 routes/index.js에서 처리
 
 const mongoURI = process.env.Local_DB_ADDRESS;
 mongoose
   .connect(mongoURI)
-  .then(() => console.log("MongoDB Connected..."))
-  .catch((err) => console.log("DB Connection Error: ", err));
+  .then(() => console.log("MongoDB 연결 성공"))
+  .catch((err) => console.log("DB 연결 실패: ", err));
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log("Server is running on port", process.env.PORT || 5000);
+  console.log("서버가 포트", process.env.PORT || 5000, "에서 실행 중입니다.");
 });
---
+++ controllers/user.controller.js
@@ -0,0 +1,29 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const userController = {};
+
+userController.createUser = async (req, res) => {
+  try {
+    const { email, password, name, level } = req.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw new Error("이미 가입된 유저입니다.");
+    }
+    const salt = await bcrypt.genSaltSync(10);
+    const hashedPassword = await bcrypt.hashSync(password, salt);
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      name,
+      level: level ? level : "customer",
+    });
+    await newUser.save();
+    res.status(200).json({ status: "createUser - 성공" });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ status: "createUser - 실패 : ", error: error.message });
+  }
+};
+module.exports = userController;
