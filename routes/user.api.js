cb5ca814baec6ee5c9bd300c0db12c4427d40b11
jieul2 <jieul2@kakao.com>
2026-02-14 15:10:01 +0900

    주석 추가

--- routes/user.api.js
+++ routes/user.api.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 // 회원가입
 router.post("/", userController.createUser);
+// 로그인한 유저 정보 조회
 router.get("/me", authController.authenticate, userController.getUser);
 
 module.exports = router;
