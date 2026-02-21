1ff1b25e3de05af0c3e798cc219a10b70bd66702
jieul2 <jieul2@kakao.com>
2026-02-21 21:04:40 +0900

    카트 기능 추가

---
+++ controllers/cart.controller.js
@@ -0,0 +1,76 @@
+const Cart = require("../models/Cart");
+
+const cartController = {};
+cartController.addItemToCart = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { productId, size, qty } = req.body;
+    // 유저를 가지고 카트 찾기
+    let cart = await Cart.findOne({ userId });
+    // 카트가 없다면 새로 만들기
+    if (!cart) {
+      cart = new Cart({
+        userId,
+      });
+      await cart.save();
+    }
+    // 이미 카트에 들어가있는 상품인지 확인하기
+    const existItem = cart.items.find(
+      (item) => item.productId.equals(productId) && item.size === size,
+    );
+    // 상품이 있다면 이미 들어가있습니다 에러 보내기
+    if (existItem) {
+      throw new Error("이미 카트에 들어가있는 상품입니다.");
+    }
+    // 상품이 없다면 카트에 상품 추가하기
+    cart.items = [...cart.items, { productId, size, qty }];
+    await cart.save();
+    res
+      .status(200)
+      .json({ status: "성공", cart, cartItemQty: cart.items.length });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ status: "장바구니 추가 실패", error: error.message });
+  }
+};
+
+cartController.getCart = async (req, res) => {
+  try {
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items",
+      populate: {
+        path: "productId",
+        model: "Product",
+      },
+    });
+    if (!cart) {
+      throw new Error("장바구니를 찾을 수 없습니다.");
+    }
+
+    res.status(200).json({ status: "성공", data: cart.items });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ status: "장바구니 조회 실패", error: error.message });
+  }
+};
+
+cartController.getCartQty = async (req, res) => {
+  try {
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+      throw new Error("장바구니를 찾을 수 없습니다.");
+    }
+
+    res.status(200).json({ status: "성공", cartItemQty: cart.items.length });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ status: "장바구니 수량 조회 실패", error: error.message });
+  }
+};
+
+module.exports = cartController;
---
+++ routes/cart.api.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const authController = require("../controllers/auth.controller");
+const cartController = require("../controllers/cart.controller");
+const router = express.Router();
+
+router.post("/", authController.authenticate, cartController.addItemToCart); // 장바구니에 상품 추가
+router.get("/", authController.authenticate, cartController.getCart); // 장바구니 아이템 가져오기
+router.get("/qty", authController.authenticate, cartController.getCartQty); // 장바구니 아이템 수량 가져오기
+
+module.exports = router;
